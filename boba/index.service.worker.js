4c4cb20b2d69f31e390113562244a5ae2bf0f471
Dan Newman <danlnewman@gmail.com>
2025-01-28 21:56:25 -0800

    boba

--- boba/index.service.worker.js
+++ boba/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738128406|2019242954';
+const CACHE_VERSION = '1738130128|3740727052';
 /** @type {string} */
 const CACHE_PREFIX = 'boba-vn-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
