ac20b6c4f78f14dc7c899dcab41c88c54b24a440
Dan Newman <danlnewman@gmail.com>
2025-08-17 23:09:53 -0700

    train

--- train2/index.service.worker.js
+++ train2/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755496935|1244652567';
+const CACHE_VERSION = '1755497359|1668346934';
 /** @type {string} */
 const CACHE_PREFIX = 'train-game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
